35c7431c257de317039ac9ef1dc72b621ae13e95
Faizul Osman <faizulosmanrabby@gmail.com>
2022-11-08 04:49:19 +0600

    single service load from db

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || "5000";
 
@@ -32,6 +32,14 @@ async function run() {
       const cursor = serviceCollection.find(query);
       const services = await cursor.toArray();
       res.send(services);
+    });
+
+    //   READ (Single Service)
+    app.get("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
     });
   } catch (error) {
     console.log(error);
