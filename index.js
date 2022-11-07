422240bc586eb0bb674e66c8ea0ac4ac0b33f07b
Faizul Osman <faizulosmanrabby@gmail.com>
2022-11-08 02:03:09 +0600

    read services from mongodb by get operation

--- index.js
+++ index.js
@@ -1,14 +1,43 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
 const port = process.env.PORT || "5000";
 
+require("dotenv").config();
+
+// middle wares
 app.use(cors());
-// app.use(express.json());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mzkazhr.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    const serviceCollection = client
+      .db("NU-Immigration")
+      .collection("services");
+
+    //   READ (Services)
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+run();
 
 app.listen(port, () => {
   console.log("Hello World server comes from port: ", port);
